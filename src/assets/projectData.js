15a7f2436261f27ecf4499d922084b7403e9b538
GummiCode <gummicodeblog@gmail.com>
2020-08-04 18:54:55 +0100

    Added an image for the QFT project.

---
+++ src/assets/projectData.js
@@ -0,0 +1,37 @@
+const projectData = [
+
+  {
+    title: "SpaceFrens",
+    summary: "Graphical representation of API data",
+    aspects: [
+      "Scripted API calls to fetch live data on people currently in space",
+      "Produced an application to display this information in a cute way",
+      "Used CSS, HTML & JavaScript to style and functionalise the app",
+      "Implemented media queries so that the application displays well on common devices",
+      "Looks nicest on mobiles ʕ◉ᴥ◉ʔ"
+    ],
+    gitHub: "https://github.com/GummiCode/spacefrens",
+    url: "https://spacefrens.herokuapp.com/",
+    image: "./images/spacefrens-demo.png",
+    featureTextColor: "#DEDDE3",
+    backgroundColor: "#687697"
+  },
+
+  {
+    title: "Queer Family Tea",
+    summary: "Social Event News & Information Site",
+    aspects: [
+      "Collaborative project to create a new site for a monthly social event",
+      "Created using React with styled-components",
+      "Blog articles procedurally generated using objects fetched from extrenal API",
+      "Heavy styling"
+    ],
+    gitHub: "https://github.com/ian-antking/q-family-dinner",
+    url: "https://queerfamilytea.com/#/",
+    image: "./images/qft-demo.png",
+    featureTextColor: "#171226",
+    backgroundColor: "#EAB39F"
+  },
+];
+
+export default projectData;
