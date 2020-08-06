08fa789019a251478cec96c66e494ac6a49182af
GummiCode <gummicodeblog@gmail.com>
2020-08-06 13:12:12 +0100

    Refactored Contact component.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,35 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FaGithub, FaAt, FaLinkedin } from "react-icons/fa";
-import { TiSocialLinkedinCircular as TiLinkedin } from "react-icons/ti";
 import FullWidthContainer from './FullWidthContainer';
 import HalfWidthContainer from './HalfWidthContainer';
 import HomeArrow from './HomeArrow';
 
 const ContactContainer = styled(FullWidthContainer)`
-  background-color: #FFFFFF;
-  align-items: start;
-`;
-
-const LeftContactContainer = styled(HalfWidthContainer)`
-  width: calc(50% - 80px);
-  height: calc(100% - 80px);
+  height: calc(100vh - 60px - 80px);
+  width: calc(100vw - 80px);
   padding: 80px 0 0 80px;
-`
-
-const RightContactContainer = styled(HalfWidthContainer)`
-  width: calc(50% - 40px);
-  padding: 0 40px 0 0;
+  background-color: #FFFFFF;
+  flex-flow: column nowrap;
+  justify-content: start;
 `
 
 const Name = styled.h1`
-  padding: 0;
   margin: 0 0 3vh 0;
   font-size: 2.6em;
 `
 
 const Title = styled.h2`
-  padding: 0;
   margin: 0 0 6vh 0;
   font-size: 2em;
 `
@@ -40,12 +30,12 @@ const LinkContainer = styled.a`
   width: 35vw;
   border-radius: 50px;
   margin: 0 0 3vh 0;
+  text-decoration: none;
   background: #FFFFFF;
   display: flex;
   flex-flow: row nowrap;
   justify-content: flex-start;
   align-items: center;
-  text-decoration: none;
 
   &:hover {
     background: #F2F2F2;
@@ -84,7 +74,6 @@ const LinkText = styled.span`
   font-size: 2em;
   font-weight: bold;
   font-style: italic;
-  text-decoration: none;
   color: #000000;
 `
 
@@ -95,57 +84,54 @@ const Contact  = ({ nextSection }) => {
       data-testid="contact"
       id="contact"
     >
-      <LeftContactContainer>
+      <Name>
+        DAVID ARROWSMITH
+      </Name>
 
-        <Name>
-          DAVID ARROWSMITH
-        </Name>
+      <Title> 
+        JUNIOR WEB DEVELOPER 
+      </Title>
 
-        <Title> 
-          JUNIOR WEB DEVELOPER 
-        </Title>
+      <LinkContainer
+        data-testid="contact-email-link"
+        href="mailto:gummicodeblog@gmail.com" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >       
+        <EmailIcon />
+        <LinkText>Email: dtarrowsmith@gmail.com</LinkText>
+      </LinkContainer>
 
-        <LinkContainer
-          data-testid="contact-email-link"
-          href="mailto:gummicodeblog@gmail.com" 
-          target="_blank" 
-          rel="noopener noreferrer"
-        >       
-          <EmailIcon />
-          <LinkText>Email: dtarrowsmith@gmail.com</LinkText>
-        </LinkContainer>
+      <LinkedinContainer
+        data-testid="contact-linkedin-link"
+        href="https://www.linkedin.com/in/david-arrowsmith/" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >       
+        <LinkedinIcon />
+        <LinkText>Linkedin: david-arrowsmith</LinkText>
+      </LinkedinContainer>
 
-        <LinkedinContainer
-          data-testid="contact-linkedin-link"
-          href="https://www.linkedin.com/in/david-arrowsmith/" 
-          target="_blank" 
-          rel="noopener noreferrer"
-        >       
-          <LinkedinIcon />
-          <LinkText>Linkedin: david-arrowsmith</LinkText>
-        </LinkedinContainer>
+      <LinkContainer
+        data-testid="contact-github-link"
+        href="https://github.com/gummicode" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >       
+        <GithubIcon />
+        <LinkText>GitHub: GummiCode</LinkText>
+      </LinkContainer>
 
-        <LinkContainer
-          data-testid="contact-github-link"
-          href="https://github.com/gummicode" 
-          target="_blank" 
-          rel="noopener noreferrer"
-        >       
-          <GithubIcon />
-          <LinkText>GitHub: GummiCode</LinkText>
-        </LinkContainer>
+    <HomeArrow 
+      homeSectionId={nextSection}
+    />
 
-      </LeftContactContainer>
-      <RightContactContainer />
-      <HomeArrow 
-        homeSectionId={nextSection}
-      />
-    </ContactContainer>
+  </ContactContainer>
   )
 };
 
 Contact.propTypes = {
   nextSection: PropTypes.string
-}
+};
 
 export default Contact;
