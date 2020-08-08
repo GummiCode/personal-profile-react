1b42f67bfe20e383beba50faaa39521d47b59352
GummiCode <gummicodeblog@gmail.com>
2020-08-09 00:02:18 +0100

    Added media queries for the About section up to & including  smmd size.

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import { up } from "styled-breakpoints";
 import ReactMarkdown from "react-markdown";
 import { BsPersonBoundingBox as AboutIcon } from "react-icons/bs";
+import breakpoints from "../styles/breakpoints";
 import SectionArrowDown from "./section-arrows/SectionArrowDown";
 import aboutText from "../assets/aboutText";
 
@@ -16,13 +18,13 @@ const AboutContainer = styled.div`
   flex-flow: column;
   justify-content: flex-start;
   align-items: flex-start;
-
 `;
 
 const TitleContainer = styled.div`
   width: 100%;
   height: 10vh;
   font-size: 4em;
+  margin: 0 0 2vh 0;
   display: flex;
   flex-flow: column nowrap;
   justify-content: center;
@@ -37,19 +39,32 @@ const StyledAboutIcon = styled(AboutIcon)`
 `;
 
 const AboutText = styled(ReactMarkdown)`
+  width:70%
   margin: 2vh 0 0 2vw;
   font-size: 1em;
+
+  ${up("sm")} {
+    font-size: 1.2em;
+  }
+
+  ${up("smmd")} {
+    font-size: 1.4em;
+  }
+
+
 `;
 
 const About = ({ nextSection }) => {
   return (
-    <AboutContainer data-testid="about" id="about">
-      <TitleContainer>
-        <StyledAboutIcon />
-      </TitleContainer>
-      <AboutText source={aboutText} />
-      <SectionArrowDown nextSectionId={nextSection} />
-    </AboutContainer>
+    <ThemeProvider theme={breakpoints}>
+      <AboutContainer data-testid="about" id="about">
+        <TitleContainer>
+          <StyledAboutIcon />
+        </TitleContainer>
+        <AboutText source={aboutText} />
+        <SectionArrowDown nextSectionId={nextSection} />
+      </AboutContainer>
+    </ThemeProvider>
   );
 };
 
--- src/styles/breakpoints.js
+++ src/styles/breakpoints.js
@@ -1,6 +1,7 @@
 const breakpoints = {
   breakpoints: {
     sm: "420px",
+    smmd: "580",
     md: "768px",
     lg: "992px",
     xl: "1200px",
