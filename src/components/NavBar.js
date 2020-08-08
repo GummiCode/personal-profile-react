5a9e4ea99bac9fdbbcf8c547c18855a5f6e74d47
GummiCode <gummicodeblog@gmail.com>
2020-08-08 03:40:56 +0100

    Applied media queries to NavBar component.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import { up } from "styled-breakpoints";
 import { FaGithub, FaAt, FaLinkedin } from "react-icons/fa";
+import breakpoints from "../styles/breakpoints";
 import BurgerMenu from "./burger-menu/BurgerMenu";
 
 const NavBarContainer = styled.div`
@@ -26,11 +28,19 @@ const LinksContainer = styled.div`
   justify-content: start;
   align-items: center;
 
+  ${up("md")} {
+    padding: 0 50px 0 0;
+  }
+
   a {
     height: 5vh;
     width: 5vh;
     border-radius: 50%;
     margin: 0 0 0 8vw;
+
+    ${up("sm")} {
+      margin: 0 0 0 33.6px;
+    }
   }
 `;
 
@@ -69,35 +79,37 @@ const LinkedinIcon = styled(FaLinkedin)`
 
 const NavBar = () => {
   return (
-    <NavBarContainer data-testid="nav-bar">
-      <BurgerMenu />
-      <LinksContainer>
-        <a
-          data-testid="nav-email-link"
-          href="mailto:gummicodeblog@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <EmailIcon />
-        </a>
-        <a
-          data-testid="nav-github-link"
-          href="https://github.com/gummicode"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GithubIcon />
-        </a>
-        <a
-          data-testid="nav-linkedin-link"
-          href="https://www.linkedin.com/in/david-arrowsmith/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <LinkedinIcon />
-        </a>
-      </LinksContainer>
-    </NavBarContainer>
+    <ThemeProvider theme={breakpoints}>
+      <NavBarContainer data-testid="nav-bar">
+        <BurgerMenu />
+        <LinksContainer>
+          <a
+            data-testid="nav-email-link"
+            href="mailto:gummicodeblog@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <EmailIcon />
+          </a>
+          <a
+            data-testid="nav-github-link"
+            href="https://github.com/gummicode"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubIcon />
+          </a>
+          <a
+            data-testid="nav-linkedin-link"
+            href="https://www.linkedin.com/in/david-arrowsmith/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedinIcon />
+          </a>
+        </LinksContainer>
+      </NavBarContainer>
+    </ThemeProvider>
   );
 };
 
