7ae0dd8ce7080ce1a210baaddeb3b96be746ed8c
GummiCode <43494071+GummiCode@users.noreply.github.com>
2020-08-10 22:31:17 +0100

    Added projectData for Platformer 00. Unfortunately localhost isn't shwing any images! It's an odd bug I've had for a little while. When pushed and hosted online it works fine so I'm pushing to check the demo image appearance.

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -1,5 +1,5 @@
-import { FaUserAstronaut, FaTransgenderAlt } from "react-icons/fa";
-
+import { FaUserAstronaut, FaTransgenderAlt, FaRegArrowAltCircleUp } from "react-icons/fa"; 
+import { GiFalling } from "react-icons/gi"; 
 const projectData = [
   {
     title: "SpaceFrens",
@@ -38,6 +38,20 @@ const projectData = [
     background: `url("./images/textures/qft.jpg")`,
     filter: "rgba(254, 74, 73, 0.8)",
     Icon: FaTransgenderAlt,
+  },
+
+  {
+    title: "Platformer 00",
+    key: "platformer00",
+    summary: "Browser-based platform game. Powered by a JavaScript event loop.",
+    aspects: [],
+    gitHub: "https://github.com/GummiCode/platformer-00",
+    url: "https://gummicode.github.io/platformer-00/",
+    image: "./images/platformer-00-sm.png",
+    textColor: "#000000",
+    background: `whitesmoke`,
+    filter: "rgba(8, 8, 14, 0)",
+    Icon: GiFalling,
   },
 ];
 
