6b17abdfe56a6ad7b9f344c4d203b32a60f2872a
GummiCode <gummicodeblog@gmail.com>
2020-08-10 14:44:04 +0100

    Updates

---
+++ precache-manifest.a92838f05152a5e532caec5db16fab77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e316a2be1a59a3a506c511e4b690509b",
+    "url": "/personal-profile-react/index.html"
+  },
+  {
+    "revision": "49720ed053c3ec56c186",
+    "url": "/personal-profile-react/static/js/2.74040e17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personal-profile-react/static/js/2.74040e17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c770adc244d20ee8241",
+    "url": "/personal-profile-react/static/js/main.d7ddbd9a.chunk.js"
+  },
+  {
+    "revision": "8fd57e1d57ad17a6e784",
+    "url": "/personal-profile-react/static/js/runtime-main.fb853879.js"
+  }
+]);
